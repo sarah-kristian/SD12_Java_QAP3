518c9217636031ac5382fddd77199f96b4e98f35
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-03-16 19:58:17 -0230

    Adding Classes for Project3 Adding Demo for Project3

--- src/SchoolAdmin/TeacherDemo.java
+++ src/SchoolAdmin/TeacherDemo.java
@@ -3,13 +3,16 @@ package SchoolAdmin;
 public class TeacherDemo {
     public static void main(String[] args) {
 
-        Person bob = new Person("Coach Bob", 27, "M");
-        System.out.println(bob);
-        Student lynne = new Student("Lynne Brooke", 16, "F", "HS95129", 3.5);
-        System.out.println(lynne);
-        Teacher mrJava = new Teacher("Duke Java", 34, "M", "Computer Science", 50000);
-        System.out.println(mrJava);
-        CollegeStudent ima = new CollegeStudent("Ima Frosh", 18, "F", "UCB123", 4.0, 1, "English");
-        System.out.println(ima);
+        Person rosie = new Person("Rosie", 28, "F");
+        Student rosha = new Student("Rosha Brooke", 10, "N", "HS95129", 3.5);
+        Teacher jeff = new Teacher("Jeff Java", 50, "M", "Computer Science", 50000);
+        CollegeStudent soren = new CollegeStudent("Soren Skillz", 18, "M", "UCB123", 4.0, 1, "English");
+
+        //display output
+        System.out.println("Meet the team:");
+        System.out.println(rosie);
+        System.out.println(rosha);
+        System.out.println(jeff);
+        System.out.println(soren);
     }
 }
